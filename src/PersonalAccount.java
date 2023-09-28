e556a4f0d9b517f9010e3186df2dd766a8d877e4
itsssrai <145002039+itsssrai@users.noreply.github.com>
2023-09-28 15:29:32 +0600

    Update PersonalAccount.java

--- src/PersonalAccount.java
+++ src/PersonalAccount.java
@@ -1,45 +1,60 @@
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Iterator;
-public class PersonalAccount {
+public class PersonalAccount
+{
     private int accountNumber;
     private String accountHolder;
     private double balance;
     private ArrayList<Amount> transactions;
-    public PersonalAccount(int accNumb, String accHolder) {
-        this.accountNumber = accNumb;
-        this.accountHolder = accHolder;
-        this.balance = 0.0;
-        this.transactions = new ArrayList();    }
-    public void deposit(double amount) {
+    public PersonalAccount(int accNumb, String accHolder)
+    {
+        accountNumber = accNumb;
+        accountHolder = accHolder;
+        balance = 0.0;
+        transactions = new ArrayList();
+    }
+    public void deposit(double amount)
+    {
         if (amount > 0.0) {
-            this.transactions.add(new Amount(amount, "Deposit"));
-            this.balance += amount;
+            transactions.add(new Amount(amount, "Deposit"));
+            balance += amount;
             System.out.println("Deposited $" + amount);
-        } else {
+        } else
+        {
             System.out.println("Error!Your deposit should be greater than 0.");
         }
     }
-    public void withdraw(double amount) {
-        if (amount > 0.0 && amount <= this.balance) {
-            this.transactions.add(new Amount(amount, "Withdrawal"));
-            this.balance -= amount;
+    public void withdraw(double amount)
+    {
+        if (amount > 0.0 && amount <= balance)
+        {
+            transactions.add(new Amount(amount, "Withdrawal"));
+            balance -= amount;
             System.out.println("Withdrawn $" + amount);
-        } else {
+        } else
+        {
             System.out.println("Error!Invalid withdrawal.");
         }
     }
-    public void printTransactionHistory() {
-        System.out.println("Transaction History for Account #" + this.accountNumber + " (" + this.accountHolder + "):");
-       for (Amount transaction : transactions)
+    public void printTransactionHistory()
+    {
+        System.out.println("Transaction History for Account #" + accountNumber + " (" + accountHolder + "):");
+        for (Amount transaction : transactions)
         {
             System.out.println(transaction.getTransactionType() + ": $" + transaction.getAmount());
-        }       
+        }
     }
-    public double getBalance() {
-        return this.balance;    }
-    public int getAccountNumber() {
-        return this.accountNumber;    }
-    public String getAccountHolder() {
-        return this.accountHolder;    }
+    public double getBalance()
+    {
+        return balance;
+    }
+    public int getAccountNumber()
+    {
+        return accountNumber;
+    }
+    public String getAccountHolder()
+    {
+        return accountHolder;
+    }
 }
