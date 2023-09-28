252c0d2490cc16f29dc95af2cb68e76a60d4be61
itsssrai <145002039+itsssrai@users.noreply.github.com>
2023-09-28 15:29:59 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public Main() {
+        public static void main(String[] args)
+        {
+            PersonalAccount rai = new PersonalAccount(220104075, "Romeo");
+            new PersonalAccount(220104073, "Juliette");
+            rai.deposit(500.0);
+            rai.withdraw(150.0);
+            rai.printTransactionHistory();
+        }
+    }
 }
-    public static void main(String[] args) {
-        PersonalAccount rai = new PersonalAccount(220104075, "Romeo");
-        new PersonalAccount(220104073, "Juliette");
-        rai.deposit(500.0);
-        rai.withdraw(150.0);
-        rai.printTransactionHistory();
-    }}
