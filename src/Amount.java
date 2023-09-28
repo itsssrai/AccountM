aa4677fa95eaa512331ac4c9231f73a5a30bc14b
itsssrai <145002039+itsssrai@users.noreply.github.com>
2023-09-28 15:30:27 +0600

    Update Amount.java

--- src/Amount.java
+++ src/Amount.java
@@ -1,11 +1,17 @@
 public class Amount {
     private double amount;
     private String transactionType;
-    public Amount(double theamount, String theTransactionType)    {
-        amount = theamount;        transactionType = theTransactionType;
+    public Amount(double theamount, String theTransactionType)
+    {
+        amount = theamount;
+        transactionType = theTransactionType;
     }
-    public double getAmount()    {
-        return amount;    }
+    public double getAmount()
+    {
+        return amount;
+    }
     public String getTransactionType()
-    {        return transactionType;
-    }}
+    {
+        return transactionType;
+    }
+}
